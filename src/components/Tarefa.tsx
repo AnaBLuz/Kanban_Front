c37433b33d0f55e3d483d0ec15468012636bbfc1
Bia Luz <anabluz08@gmail.com>
2025-05-16 01:12:13 -0300

    card Nova Tarefa

--- src/components/Tarefa.tsx
+++ src/components/Tarefa.tsx
@@ -1,19 +1,33 @@
 import styled from 'styled-components';
 import { useState } from 'react';
+import diskette from "../assets/diskette.png"
 
 export default function Tarefa() {
     const [valor,setValor] = useState("");
 
-    const handleChange = (event) => setValor(event.target.value)
+    const handleChange = (event) => {
+      setValor(event.target.value)
+    };
+
+    function salvaTarefa(){
+      
+      setValor("")
+    }
     
 
     return (
-        <TarefaContainer>
-             
-             <input type="text" 
+        <TarefaContainer>       
+             <input 
+             type="text" 
              placeholder="Nova Tarefa"
              onChange={handleChange}
+             value={valor}
              />
+             <img src={diskette}  
+                height="30px" 
+                width="30px"
+                onClick={salvaTarefa}
+                />
         </TarefaContainer>
       
     );
@@ -25,10 +39,17 @@ height: 100px;
 border-radius: 24px;
 background-color: orange;  
 margin: 30px;
+display:flex;
+align-items:center;
+
    input{
-    width:100%;
-    height: 100%;
+    width:80%;
+    height: 50%;
     border-radius: 24px;
     background-color: orange;
+    margin:10px;
    }
+  img{
+    cursor:pointer;
+  }
 `
